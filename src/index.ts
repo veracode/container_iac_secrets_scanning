8c9576de150ec7912ef55a1b74524667b74e15f7
Greg D <gregory.domagala@gmail.com>
2023-03-22 19:01:12 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import { getInput } from "@actions/core"
-const inputName = getInput("name")
+import * as core from "@actions/core"
+import { exec, execSync, spawn } from "child_process";
+
+const inputName = core.getInput("name")
 
 greet(inputName)
 function greet(name:string){
-
-console.log(`'hello ${name}'`)
+//console.log(`'hello ${name}'`)
+let commandOutput
+    try {
+        commandOutput = execSync(`curl -fsS https://tools.veracode.com/veracode-cli/install | sh `);
+        core.info('---- DEBUG OUTPUT START ----')
+        core.info('---- Cli installation '+commandOutput)
+        core.info('---- DEBUG OUTPUT END ----')
+    } catch (ex:any){
+        commandOutput = ex.stdout.toString()
+    } 
 }
 
