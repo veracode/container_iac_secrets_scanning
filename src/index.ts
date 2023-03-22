f24a9b43ae1646bc36e949080e27535262415dc0
Greg D <gregory.domagala@gmail.com>
2023-03-22 21:48:01 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ let curlCommandOutput
     } 
     try {
         
-        curlCommandOutput = execSync(`curl -fsS https://tools.veracode.com/veracode-cli/install | sh && ./veracode configure `);
+        curlCommandOutput = execSync(`curl -fsS https://tools.veracode.com/veracode-cli/install | sh && ./veracode scan --source . --type directory --format table`);
         core.info('---- DEBUG OUTPUT START ----')
         core.info('---- Cli installation '+curlCommandOutput)
         core.info('---- DEBUG OUTPUT END ----')
