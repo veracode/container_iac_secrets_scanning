baa9f61d1417b59eb0757ab5110a701fbba7ba17
Greg D <gregory.domagala@gmail.com>
2023-03-22 23:06:22 +0100

    test

--- dist/index.js
+++ dist/index.js
@@ -25,9 +25,9 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const child_process_1 = require("child_process");
-const vid = core.getInput("vid");
-const vkey = core.getInput("vkey");
-const path = core.getInput("path");
+const vid = core.getInput("vid", { required: true });
+const vkey = core.getInput("vkey", { required: true });
+const path = core.getInput("path", { required: true });
 ContainerScan(vid, vkey, path);
 function ContainerScan(vid, vkey, path) {
     console.log(`'Path :  ${path}'`);
@@ -36,10 +36,8 @@ function ContainerScan(vid, vkey, path) {
         process.env.VERACODE_API_KEY_ID = vid;
         process.env.VERACODE_API_KEY_SECRET = vkey;
         curlCommandOutput = (0, child_process_1.execSync)(`curl -fsS https://tools.veracode.com/veracode-cli/install | sh && ./veracode scan --source ${path} --type directory --format table`);
-        core.info('---- DEBUG OUTPUT START ----');
-        core.info('---- Cli installation ' + curlCommandOutput);
+        core.info(`${curlCommandOutput}`);
         core.notice(`${curlCommandOutput}`);
-        core.info('---- DEBUG OUTPUT END ----');
     }
     catch (ex) {
         curlCommandOutput = ex.stdout.toString();
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import * as core from "@actions/core"
 import * as github from "@actions/github"
-import { execSync} from "child_process";
+import { execSync } from "child_process";
 import { env } from "process";
 
-const vid = core.getInput("vid")
-const vkey = core.getInput("vkey")
-const path = core.getInput("path")
+const vid = core.getInput("vid", {required:true})
+const vkey = core.getInput("vkey", {required:true})
+const path = core.getInput("path", {required:true})
 
 
 ContainerScan(vid,vkey,path)
@@ -16,10 +16,8 @@ let curlCommandOutput
         process.env.VERACODE_API_KEY_ID= vid
         process.env.VERACODE_API_KEY_SECRET= vkey
         curlCommandOutput = execSync(`curl -fsS https://tools.veracode.com/veracode-cli/install | sh && ./veracode scan --source ${path} --type directory --format table`);
-        core.info('---- DEBUG OUTPUT START ----')
-        core.info('---- Cli installation '+curlCommandOutput)
+        core.info(`${curlCommandOutput}`)
         core.notice(`${curlCommandOutput}`)
-        core.info('---- DEBUG OUTPUT END ----')
     } catch (ex:any){
         curlCommandOutput = ex.stdout.toString()
     } 
