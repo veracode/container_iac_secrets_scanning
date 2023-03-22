692b0c83b942b6219a8f95aed167a4524d785bba
Greg D <gregory.domagala@gmail.com>
2023-03-22 18:52:38 +0100

    test

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@actions/core");
 const inputName = (0, core_1.getInput)("name");
+greet(inputName);
 function greet(name) {
     console.log('hello ${name}');
 }
