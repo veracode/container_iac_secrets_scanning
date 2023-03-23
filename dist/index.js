86a0f1c589e3a071771723354580e13c9ef2681d
Greg D <gregory.domagala@gmail.com>
2023-03-23 10:39:02 +0100

    test

--- dist/index.js
+++ dist/index.js
@@ -25,17 +25,43 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const child_process_1 = require("child_process");
+const process_1 = require("process");
 const vid = core.getInput("vid", { required: true });
 const vkey = core.getInput("vkey", { required: true });
 const path = core.getInput("path", { required: true });
-ContainerScan(vid, vkey, path);
-function ContainerScan(vid, vkey, path) {
+const format = core.getInput("format", { required: true });
+const scanType = core.getInput("scanType", { required: true });
+const exportfile = core.getInput("export", { required: true });
+ContainerScan(vid, vkey, path, format, scanType, exportfile);
+async function ContainerScan(vid, vkey, path, format, scanType, exportfile) {
     console.log(`'Path :  ${path}'`);
     let curlCommandOutput;
     try {
-        process.env.VERACODE_API_KEY_ID = vid;
-        process.env.VERACODE_API_KEY_SECRET = vkey;
-        curlCommandOutput = (0, child_process_1.execSync)(`curl -fsS https://tools.veracode.com/veracode-cli/install | sh && ./veracode scan --source ${path} --type directory --format table`);
+        let ext;
+        process_1.env.VERACODE_API_KEY_ID = vid;
+        process_1.env.VERACODE_API_KEY_SECRET = vkey;
+        if (format = 'json')
+            ext = '.json';
+        if (format = 'table')
+            ext = '.txt';
+        if (format = 'cyclonedx')
+            ext = '.xml';
+        if (exportfile = 'true') {
+            curlCommandOutput = (0, child_process_1.execSync)(`curl -fsS https://tools.veracode.com/veracode-cli/install | sh && ./veracode ${scanType} --source ${path} --type directory --format ${format} --output results${ext} `);
+            //store output files as artifacts
+            const artifact = require('@actions/artifact');
+            const artifactClient = artifact.create();
+            const artifactName = 'Veracode Container Scanning Results';
+            const files = `'results${ext}'`;
+            const rootDirectory = process.cwd();
+            const options = {
+                continueOnError: true
+            };
+            const uploadResult = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options);
+        }
+        else {
+            curlCommandOutput = (0, child_process_1.execSync)(`curl -fsS https://tools.veracode.com/veracode-cli/install | sh && ./veracode ${scanType} --source ${path} --type directory --format ${format}`);
+        }
         core.info(`${curlCommandOutput}`);
         core.notice(`${curlCommandOutput}`);
     }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as core from "@actions/core"
+import {create, UploadOptions} from '@actions/artifact'
 import * as github from "@actions/github"
 import { execSync } from "child_process";
 import { env } from "process";
@@ -6,16 +7,41 @@ import { env } from "process";
 const vid = core.getInput("vid", {required:true})
 const vkey = core.getInput("vkey", {required:true})
 const path = core.getInput("path", {required:true})
+const format = core.getInput("format", {required:true})
+const scanType = core.getInput("scanType", {required:true})
+const exportfile = core.getInput("export", {required:true})
 
 
-ContainerScan(vid,vkey,path)
-function ContainerScan(vid:string, vkey:string, path:string){
+ContainerScan(vid, vkey, path, format, scanType, exportfile)
+async function ContainerScan(vid:string, vkey:string, path:string, format:string, scanType:string, exportfile:string){
 console.log(`'Path :  ${path}'`)
 let curlCommandOutput
     try {
-        process.env.VERACODE_API_KEY_ID= vid
-        process.env.VERACODE_API_KEY_SECRET= vkey
-        curlCommandOutput = execSync(`curl -fsS https://tools.veracode.com/veracode-cli/install | sh && ./veracode scan --source ${path} --type directory --format table`);
+        let ext 
+        env.VERACODE_API_KEY_ID= vid
+        env.VERACODE_API_KEY_SECRET= vkey
+        if(format='json') ext='.json'
+        if(format='table') ext='.txt'
+        if(format='cyclonedx') ext='.xml'
+
+        if(exportfile='true') {
+            curlCommandOutput = execSync(`curl -fsS https://tools.veracode.com/veracode-cli/install | sh && ./veracode ${scanType} --source ${path} --type directory --format ${format} --output results${ext} `);
+            //store output files as artifacts
+            const artifact = require('@actions/artifact');
+            const artifactClient = artifact.create()
+            const artifactName = 'Veracode Container Scanning Results';
+            const files = `'results${ext}'`
+
+            const rootDirectory = process.cwd()
+            const options = {
+                continueOnError: true
+            }
+
+            const uploadResult = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
+            }
+        else{
+            curlCommandOutput = execSync(`curl -fsS https://tools.veracode.com/veracode-cli/install | sh && ./veracode ${scanType} --source ${path} --type directory --format ${format}`);
+        }
         core.info(`${curlCommandOutput}`)
         core.notice(`${curlCommandOutput}`)
     } catch (ex:any){
