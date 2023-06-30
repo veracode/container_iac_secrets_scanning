cf7e194ca737d6882603656b62f6558b8b0db83b
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2023-06-30 17:30:35 +0200

    upodates

--- src/containerScan.ts
+++ src/containerScan.ts
@@ -22,10 +22,10 @@ export async function ContainerScan(parameters:any) {
 
     let results_file = ""
     if ( parameters.format == "json" ){
-      let results_file = 'results.json'
+      results_file = 'results.json'
     }
     else {
-      let results_file = 'results.txt'
+      results_file = 'results.txt'
     }
 
     //generate command to run
