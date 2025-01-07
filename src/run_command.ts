e68239ed050113a19fe5c13089039d21ec257573
veeranjaneya reddy <vreddy@veracode.com>
2025-01-07 13:53:31 +0530

    removed console in if condition

--- src/run_command.ts
+++ src/run_command.ts
@@ -31,7 +31,6 @@ export async function run_cli(command:string, debug:any, resultsfile:any, failBu
         }
 
         const failureMessage = `Veracode CLI scan failed. Exit code: ${error.status}, Command: ${scanCommand}`;
-        core.info(`failBuildOnError: ${failBuildOnError}`);
         if (failBuildOnError === true) {
             core.setFailed(failureMessage);
             core.info(`Note: Build failed due to break_build_on_error flag being set to true.`)
