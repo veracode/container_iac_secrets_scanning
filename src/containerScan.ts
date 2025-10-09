def2442d12be652403c66fe73931f13d9c12c533
Shailesh Hande <shande@veracode.com>
2025-10-09 14:07:56 +0530

    cofigured based url based on the action running on env

--- src/containerScan.ts
+++ src/containerScan.ts
@@ -128,7 +128,8 @@ export async function ContainerScan(parameters:any) {
       core.info("This run is part of a PR, should add some PR comment")
 
       try {
-        const octokit = github.getOctokit(parameters.token);
+        const baseUrl = process.env.GITHUB_API_URL || 'https://api.github.com';
+        const octokit = github.getOctokit(parameters.token, { baseUrl });
 
         const context = github.context
         const repository:any = process.env.GITHUB_REPOSITORY
