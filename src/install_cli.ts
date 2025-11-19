34b47879966b0738e0bcecda35a03f94ad4e7e54
Shipra Kumari <skumari@veracode.com>
2025-11-19 19:43:51 +0530

    IaC Scan, Window Runner changes

--- src/install_cli.ts
+++ src/install_cli.ts
@@ -1,19 +1,33 @@
 import * as core from "@actions/core"
-import { execSync,exec } from "child_process";
+import { execSync, exec } from "child_process";
 
+const runnerOS = process.env.RUNNER_OS;
+export async function install_cli(parameters: any) {
+  if (runnerOS == 'Windows') {
+    const psCommand = `Set-ExecutionPolicy AllSigned -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://tools.veracode.com/veracode-cli/install.ps1'))`
+    const curlCommandOutput = execSync(`powershell.exe -NoProfile -Command "${psCommand}"`, { stdio: 'inherit' })
+    if (parameters.debug == "true") {
+      core.info('#### DEBUG START ####')
+      core.info('intall_cli.ts - command output')
+      core.info('command output : ' + curlCommandOutput)
+      core.info('#### DEBUG END ####')
+    }
+    core.info(`${curlCommandOutput}`)
 
-export async function install_cli(parameters:any) {
-
+  }
+  else {
     let installCommand = `cd ..;mkdir veracode-cli; cd veracode-cli; curl -fsS https://tools.veracode.com/veracode-cli/install | sh`
     core.info('Install command :' + installCommand)
     let curlCommandOutput = execSync(installCommand)
 
-    if ( parameters.debug == "true" ){
-        core.info('#### DEBUG START ####')
-        core.info('intall_cli.ts - command output')
-        core.info('command output : '+curlCommandOutput)
-        core.info('#### DEBUG END ####')
-      }
+    if (parameters.debug == "true") {
+      core.info('#### DEBUG START ####')
+      core.info('intall_cli.ts - command output')
+      core.info('command output : ' + curlCommandOutput)
+      core.info('#### DEBUG END ####')
+    }
     core.info(`${curlCommandOutput}`)
+
+  }
 
 }
