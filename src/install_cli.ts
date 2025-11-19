6fdab4cba86ec23a972dd154587bf75fccf0c256
Shipra Kumari <skumari@veracode.com>
2025-11-19 21:59:12 +0530

    Windows Runner chnages for IaC Scan

--- src/install_cli.ts
+++ src/install_cli.ts
@@ -1,9 +1,22 @@
 import * as core from "@actions/core"
 import { execSync,exec } from "child_process";
 
+const runnerOS = process.env.RUNNER_OS;
 
 export async function install_cli(parameters:any) {
+  if (runnerOS == 'Windows') {
+    const psCommand = `Set-ExecutionPolicy AllSigned -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://tools.veracode.com/veracode-cli/install.ps1'))`
+    const curlCommandOutput = execSync(`powershell.exe -NoProfile -Command "${psCommand}"`, { stdio: 'inherit' })
+    if (parameters.debug == "true") {
+      core.info('#### DEBUG START ####')
+      core.info('intall_cli.ts - command output')
+      core.info('command output : ' + curlCommandOutput)
+      core.info('#### DEBUG END ####')
+    }
+    core.info(`${curlCommandOutput}`)
 
+  }
+  else {
     let installCommand = `cd ..;mkdir veracode-cli; cd veracode-cli; curl -fsS https://tools.veracode.com/veracode-cli/install | sh`
     core.info('Install command :' + installCommand)
     let curlCommandOutput = execSync(installCommand)
@@ -16,4 +29,5 @@ export async function install_cli(parameters:any) {
       }
     core.info(`${curlCommandOutput}`)
 
+}
 }
